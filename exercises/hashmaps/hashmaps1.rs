6f706aa4f783f041233c280bbc44fd5c8aeddbc8
sym <dear.zomb@gmail.com>
2023-04-04 18:45:00 +0000

    hashmaps, modules, options

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,17 +10,22 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert("strawberry".into(), 2);
+    basket.insert("apple".into(), 3);
+    basket.insert("orange".into(), 4);
+    basket.insert("pear".into(), 5);
+    basket.insert("avocado".into(), 6);
 
     basket
 }
--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -13,7 +13,7 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +39,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the basket.
         // Note that you are not allowed to put any type of fruit that's already
         // present!
+        basket.entry(fruit).or_insert(2);
     }
 }
 
