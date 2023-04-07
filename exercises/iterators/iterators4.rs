ecaa133406db0d1274e197df4b9f43edfcf25a14
sym <dear.zomb@gmail.com>
2023-04-07 15:13:05 +0000

    lifetimes, iterators

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,7 +1,9 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
+
+use std::ops::Mul;
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +15,7 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    (1..=num).fold(1, Mul::mul)
 }
 
 #[cfg(test)]
