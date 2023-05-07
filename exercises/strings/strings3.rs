7c55d27c3ef1bdbf0c403cd1b8a730c2595d240a
sym <dear.zomb@gmail.com>
2023-05-07 17:42:43 +0000

    strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,45 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let l = input.len();
+    let mut s = 0;
+    let mut e = l;
+    while s < e && &input[s..s+1] == " " {
+        s += 1;
+    }
+    while s < e && &input[e - 1..e] == " " {
+        e -= 1;
+    }
+    input[s..e].into()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut s = String::new();
+    let l = input.len();
+    let mut i = 0;
+    let pattern = "cars";
+    let pl = pattern.len();
+    let target = "balloons";
+    while i < l {
+        if i + pl <= l && &input[i..i + pl] == pattern {
+            s += target;
+            i += pl;
+        } else {
+            s += &input[i..i + 1];
+            i += 1
+        }
+    }
+    s
 }
 
 #[cfg(test)]
